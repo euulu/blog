e96e0e8d77bcc60ea63911009be84bf9d0c7fa74
anegin <anegamega@gmail.com>
2023-05-03 18:03:33 +0300

    Use defaul image for post if no uploaded

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,15 @@ class Post < ApplicationRecord
   validates :body, presence: true, length: { minimum: 10 }
 
   has_one_attached :image
+  after_commit :add_post_default_img, on: %i[create update]
+
+  def add_post_default_img
+    return if image.attached?
+
+    image.attach(
+      io: File.open(Rails.root.join('app', 'assets', 'images', 'default-post.jpg')),
+      filename: 'default-post.jpg',
+      content_type: 'image/jpg'
+    )
+  end
 end
