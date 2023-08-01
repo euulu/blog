64b43c283b2cadc07e566a3e823e2b4d95201c27
anegin <anegamega@gmail.com>
2023-08-01 16:30:20 +0300

    Add comments redirect on create.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,12 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
-    redirect_to post_path(@post)
+
+    if @comment.save
+      redirect_to @post
+    else
+      render @post, status: :unprocessable_entity
+    end
   end
 
   private
